ad0b98c5527a59fbd08ad20104d8273fe83e04d5
Yeonhan Jung <yt4307@gmail.com>
2025-01-05 18:47:48 +0900

    chore: assetPrefix 추가를 위한 next.config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
   output: "export",
+  assetPrefix: isDev ? undefined : "https://yt4307.github.io/chulendar/",
 };
 
 export default nextConfig;
